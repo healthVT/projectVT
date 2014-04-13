cf75d2e08e09e8f648b90c711fddcda82a453400
penny <propenny31@yahoo.com.tw>
2014-04-12 23:59:15 -0400

    change test

--- src/com/example/projectVT/mainActivity.java
+++ src/com/example/projectVT/mainActivity.java
@@ -22,7 +22,7 @@ public class mainActivity extends Activity {
     TableLayout infoTable;
     LinearLayout vitaminList;
     List<Integer> vitaminArray = new ArrayList<Integer>();
-    //this is test change
+    //this is test change//penny change
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
